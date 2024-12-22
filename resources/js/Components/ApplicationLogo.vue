3381b736efbfed57d916291bb5993fdffc1db258
mindfulcoder49 <alex.g.alcivar49@gmail.com>
2024-12-21 20:45:37 -0500

    colors and logo update

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,5 +1,5 @@
 <template>
     <!-- Laravel Breeze with vue and inertia link to logo.jpeg in public images directory -->
-    <img src="/images/logo.png" alt="Logo" />
+    <img src="/images/logoSplash.png" alt="Logo" class="logo" />
   </template>
   
--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -15,16 +15,16 @@ const showingNavigationDropdown = ref(false);
 <template>
     <div>
         <div class="min-h-screen">
-            <nav class="bg-white border-b border-gray-100">
+            <nav class="bg-white border-b border-gray-100 w-full">
                 <!-- Primary Navigation Menu -->
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="flex justify-between h-16">
-                        <div class="flex">
+                <div class="w-auto m-5">
+                    <div class="flex justify-between h-24">
+                        <div class="flex ">
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
                                 <Link :href="route('map.index')">
                                     <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800"
+                                        class="block h-24 w-auto fill-current text-gray-800"
                                     />
                                 </Link>
                             </div>
