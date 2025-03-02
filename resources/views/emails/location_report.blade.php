537e55320466dfce9609f3de5c6eec47d5608c57
mindfulcoder49 <alex.g.alcivar49@gmail.com>
2025-03-02 16:12:51 -0500

    update prompts and report to all be in specified language

--- resources/views/emails/location_report.blade.php
+++ resources/views/emails/location_report.blade.php
@@ -1,17 +1,6 @@
 @component('mail::message')
 
-
-# Hello,
-
-Here's your daily report for {{ $location->address }}:
-
 {{ $report }}
-
-@component('mail::button', ['url' => url('/profile')])
-Update Your Location Settings
-@endcomponent
-
-Thanks,
 
 {{ config('app.name') }}
 @endcomponent
