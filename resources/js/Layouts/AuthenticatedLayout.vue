83701b3cda1316b5b251c710f5815f34241793ac
mindfulcoder49 <alex.g.alcivar49@gmail.com>
2025-03-02 17:09:51 -0500

    try to fix logout

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -12,10 +12,13 @@ const showingNavigationDropdown = ref(false);
 
 //function to replace :href="route('logout')" with a function that logs out the user and then refreshes the page
 function logout() {
-    //log out the user
-    axios.post('/logout').then(() => {
-        window.location.reload();
-    });
+    //log out the user and wait for the response to come back
+    $response = axios.post('/logout')
+
+    //if the response is successful, refresh the page
+    if ($response.status == 200) {
+        location.reload();
+    }
 }
 
 //comment 
