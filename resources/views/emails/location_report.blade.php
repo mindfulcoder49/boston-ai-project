266edf7c90de714a1d64412304de0e08a1267f9e
mindfulcoder49 <alex.g.alcivar49@gmail.com>
2025-03-02 14:38:36 -0500

    update

--- resources/views/emails/location_report.blade.php
+++ resources/views/emails/location_report.blade.php
@@ -1,19 +1,17 @@
 @component('mail::message')
 
-<div class="prose">
+
 # Hello,
 
 Here's your daily report for {{ $location->address }}:
 
-{{-- Use the "prose" class for better typography. --}}
-
 {{ $report }}
-</div>
 
 @component('mail::button', ['url' => url('/profile')])
 Update Your Location Settings
 @endcomponent
 
-Thanks,<br>
+Thanks,
+
 {{ config('app.name') }}
 @endcomponent
